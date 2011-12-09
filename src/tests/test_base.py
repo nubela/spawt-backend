191178443a6139df822100dc55e1a7ea625ff15f
nubela <nubela@ctrleff.com>
2011-12-09 20:33:50 +0800

    added test util methods for creating test users

--- src/tests/test_base.py
+++ src/tests/test_base.py
@@ -34,3 +34,42 @@ class TestBase(unittest.TestCase):
     def create_facebook_test_user(self):
         self.has_facebook_test_user = True
         return create_test_user(APP_ID, self.get_app_access_token())
+    
+    def create_saved_test_user(self):
+        from util.util import random_string
+        from action.user import save_user
+        from collections import namedtuple
+        
+        #create and save fb user
+        authcode = random_string()
+        test_user = self.create_facebook_test_user()
+        fb_info, user = save_user(test_user["access_token"], authcode)
+        
+        User = namedtuple("User", ("authcode", "fb_test_user", "fb_info", "user_obj", "checkpoint_obj", "user_checkpoint_obj"))
+        return User(authcode, test_user, fb_info, user)
+    
+    def create_saved_test_user_with_checkpoint(self):
+        """
+        Creates a test user, with a checkpoint
+        """
+        from collections import namedtuple
+        from util.util import random_string
+        from action.user import save_user
+        from tests.action.test_checkpoint import CheckpointTests
+        from action.checkpoint import add_checkpoint
+        from action.user_checkpoint import add_checkpoint_to_user
+        
+        #create and save fb user
+        authcode = random_string()
+        test_user = self.create_facebook_test_user()
+        fb_info, user = save_user(test_user["access_token"], authcode)
+        
+        #create user checkpoint
+        mock_cp_data = CheckpointTests.mock_checkpoint_data(user.id)
+        checkpoint = add_checkpoint(*mock_cp_data)
+        user_checkpoint = add_checkpoint_to_user(user, checkpoint)
+        
+        #create namedtuple to store
+        User = namedtuple("User", ("authcode", "fb_test_user", "fb_info", "user_obj", "checkpoint_obj", "user_checkpoint_obj"))
+        return User(authcode, test_user, fb_info, user, checkpoint, user_checkpoint)
+        
